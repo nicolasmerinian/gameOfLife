534ae9560ccc7bae328e1634feb6a7a6c0c0cb60
Nicolas <nicolas.merinian@gmail.com>
2017-12-12 21:22:32 +0100

    draw tiled board

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var Gol = function(size) {
+var Gol = function(size, cellSize) {
 	var container = document.getElementById('container');
 	this.canvas = document.createElement('canvas');
 	this.canvas.setAttribute('width', size);
@@ -10,15 +10,32 @@ var Gol = function(size) {
 	this.canvas.id = 'canvas';
 	container.appendChild(this.canvas);
 	
+	this.cellSize = cellSize;
 	this.ctx = this.canvas.getContext('2d');
 	this.width = this.canvas.width;
 	this.height = this.canvas.height;
 }
 
 Gol.prototype.draw = function draw() {
-	this.ctx.fillStyle = 'red';
-	this.ctx.fillRect(0, 0, this.width, this.height);
+	this.drawBoard();
 }
 
-var gol = new Gol(200);
+Gol.prototype.drawBoard = function drawBoard() {
+	this.ctx.fillStyle = '#fff';
+	this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+	this.ctx.strokeStyle = '#000';
+	this.ctx.beginPath();
+	for (var i = 0; i < this.width / this.cellSize; i++) {
+		this.ctx.moveTo(i * this.cellSize, 0);
+		this.ctx.lineTo(i * this.cellSize, this.height);
+	}
+	for (var j = 0; j < this.height / this.cellSize; j++) {
+		this.ctx.moveTo(0, j * this.cellSize);
+		this.ctx.lineTo(this.width, j * this.cellSize);
+	}
+	this.ctx.closePath();
+	this.ctx.stroke();
+}
+
+var gol = new Gol(200, 40);
 gol.draw();
