c4041f7d773c48dcd3fa9ea517d560ca142b15c7
Nicolas <nicolas.merinian@gmail.com>
2017-12-12 20:19:48 +0100

    created class + canvas init

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-console.log('hello world');
+var Gol = function(width, height) {
+	var container = document.getElementById('container');
+	this.canvas = document.createElement('canvas');
+	this.canvas.setAttribute('width', width);
+	this.canvas.setAttribute('height', height);
+	this.canvas.style.border = '1px solid #000';
+	this.canvas.style.position = 'absolute';
+	this.canvas.style.top = '0';
+	this.canvas.style.left = '1px';
+	this.canvas.id = 'canvas';
+	container.appendChild(this.canvas);
+	
+	this.ctx = this.canvas.getContext('2d');
+	this.width = this.canvas.width;
+	this.height = this.canvas.height;
+}
+
+new Gol(200, 200);
